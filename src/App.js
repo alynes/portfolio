07596bd1da65e224e585f1759f67bd4f0558e29a
Austin Lynes <austinlynes7@gmail.com>
2022-09-16 21:42:10 -0500

    Update resume formatting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import { CSSTransition } from 'react-transition-group';
 
 import Layout from './components/Layout/index.js';
